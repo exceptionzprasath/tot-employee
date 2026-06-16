91117c9180b4ace8afae1737f8936c6605219d01
exceptionzprasath <exceptionzprasath@gmail.com>
2026-06-16 09:21:45 +0530

    added 16-6

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Central source of truth for the backend URL
-export const API_BASE_URL = 'https://settlo-tot-backend-production.up.railway.app';
+export const API_BASE_URL = 'https://api.foodman.shop';
 // export const API_BASE_URL = 'https://c769-2401-4900-93d2-5e42-6c24-1695-9791-101f.ngrok-free.app';
 
 const api = axios.create({
